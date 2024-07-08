a23867a0dc72ad6e3f1d7304808bf021fed61eb3
Shreya Srivastava <160320068+Peaks221@users.noreply.github.com>
2024-07-09 00:00:44 +0530

    Add files via upload

---
+++ google-meet-clone-main/hooks/useMediaStream.js
@@ -0,0 +1,28 @@
+const { useState, useEffect, useRef } = require("react");
+
+const useMediaStream = () => {
+  const [state, setState] = useState(null);
+  const isStreamSet = useRef(false);
+  useEffect(() => {
+    if (isStreamSet.current) return;
+    isStreamSet.current = true;
+    (async function initStream() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: true,
+        });
+        console.log("setting your stream");
+        setState(stream);
+      } catch (e) {
+        console.log("error in setting media stream", e);
+      }
+    })();
+  }, []);
+
+  return {
+    stream: state,
+  };
+};
+
+export default useMediaStream;
